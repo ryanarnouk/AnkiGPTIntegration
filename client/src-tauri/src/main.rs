29c0521ea1c904f5e36e7f5c5d53dfebfdbec465
Ryan <azbo400@gmail.com>
2024-05-07 12:22:15 -0700

    fixed sidecar setup

--- client/src-tauri/src/main.rs
+++ client/src-tauri/src/main.rs
@@ -1,7 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {    
+use tauri::api::process::Command;
+use tauri::api::process::CommandEvent;
+
+fn main() {
+  let (mut rx, mut _child) = Command::new_sidecar("api")
+    .expect("failed to create sidecar api")
+    .spawn()
+    .expect("failed to spawn sidecar");
+
+  tauri::async_runtime::spawn(async move {
+    while let Some(event) = rx.recv().await {
+      if let CommandEvent::Stdout(line) = event {
+        println!("{}", line)
+      }
+    }
+  });
+  
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
